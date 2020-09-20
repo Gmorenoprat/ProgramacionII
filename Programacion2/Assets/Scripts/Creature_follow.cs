f253b2552ce30f8001e5cc46ad645ea1e311b097
Munguia Mariano <63915986+mungored88@users.noreply.github.com>
2020-09-19 21:20:10 -0300

    subida sin craturas
    subida sin carpeta de minions

---
+++ Programacion2/Assets/Scripts/Creature_follow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Creature_follow : MonoBehaviour
+{
+    public Transform ObjetToFollow;
+    public float speed;
+    Vector3 Direction;
+    const float EPSILON= 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Direction = (ObjetToFollow.position - transform.position).normalized;
+        
+        if((transform.position - ObjetToFollow.position).magnitude >EPSILON)
+        {
+            transform.Translate(Direction * Time.deltaTime * speed);
+        }
+    }
+}
---
+++ Programacion2/Assets/Scripts/sky_loop.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sky_loop : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<MeshRenderer>().material.mainTextureOffset += Vector2.right * speed * Time.deltaTime;
+    }
+}
