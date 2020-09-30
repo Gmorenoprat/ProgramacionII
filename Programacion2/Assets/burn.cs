a3e7e90406f4c10166cf41550c9e52aa854247a9
mariano <marianoa.munguia@gmail.com>
2020-09-30 18:05:29 -0300

    enemy anim ok

---
+++ Programacion2/Assets/burn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class burn : MonoBehaviour
+{
+    public int damage;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("me quemo");
+        if (other.gameObject.layer == 9)
+        {
+
+            other.gameObject.GetComponent<Character_Controler>().Life -= damage;
+        }
+    }
+}
