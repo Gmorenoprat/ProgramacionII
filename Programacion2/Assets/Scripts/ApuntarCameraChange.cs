7fdcf5b0f36bf8986853e0427c37aa22ca2f4500
gonzalo.morenop <gonzalo.morenop@davinci.edu.ar>
2020-09-30 21:30:45 -0300

    CameraChange y Mira

--- Programacion2/Assets/Scripts/ApuntarCameraChange.cs
+++ Programacion2/Assets/Scripts/ApuntarCameraChange.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,19 +9,27 @@ public class ApuntarCameraChange : MonoBehaviour
     private bool estaApuntando = false;
     public GameObject mira;
 
-    public GameObject thirdPersonCam;
-    public GameObject apuntarCam;
+    //public GameObject thirdPersonCam;
+   // public GameObject apuntarCam;
+
+    public CinemachineVirtualCamera apuntarCam;
+    //public CinemachineFreeLook thirdPersonCam;
 
 
     // Update is called once per frame
     void Update()
     {
-       //if (Input.GetButtonDown("Fire2")) {
-       //    estaApuntando = !estaApuntando;
-       //    mira.SetActive(estaApuntando);
-       //    thirdPersonCam.SetActive(!estaApuntando);
-       //    apuntarCam.SetActive(estaApuntando);
-       //}
+       if (Input.GetKeyDown(KeyCode.Mouse1)) {
+           estaApuntando = true;
+            apuntarCam.Priority += 2;
+
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1)) {
+           estaApuntando = false;
+            apuntarCam.Priority -= 2;
+        }
+        
+         mira.SetActive(estaApuntando);
 
     }
 }
--- Programacion2/Assets/Scripts/ThirdPersonMovement.cs
+++ Programacion2/Assets/Scripts/ThirdPersonMovement.cs
@@ -15,6 +15,8 @@ public class ThirdPersonMovement : MonoBehaviour
     public Rigidbody player_rb;
     public Animator animator;
 
+    public GameObject espinaDorsal;
+
     private void Start()
     {
         playersound = GetComponent<sound>();
@@ -29,8 +31,10 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        bool estaApuntando = animator.GetBool("estaApuntando");
 
-        if (direction.magnitude >= 0.1f)
+        //MovimientoSinApuntar
+        if (direction.magnitude >= 0.1f && !estaApuntando)
         {
 
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -40,6 +44,9 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             player_rb.velocity = moveDir * speed;
 
+            animator.SetFloat("movementSpeed", direction.magnitude);
+
+
             animator.Play("Walk");
             // playersound.SoundPlay(playersound.clips[1]);
         }
@@ -48,6 +55,49 @@ public class ThirdPersonMovement : MonoBehaviour
             animator.SetFloat("movementSpeed", 0f);
             player_rb.velocity = Vector3.zero;
         }
+
+        if (direction.magnitude >= 0.1f && estaApuntando)
+        {
+
+            if(horizontal < 0)
+            {
+                player_rb.velocity = transform.right * -speed;
+                animator.Play("AimWalkLeft");
+                animator.SetFloat("movementSpeed", direction.magnitude);
+
+            }
+            if (horizontal > 0) 
+            {
+                player_rb.velocity = transform.right * speed;
+                animator.Play("AimWalkRight");
+                animator.SetFloat("movementSpeed", direction.magnitude);
+
+            }
+            if (vertical < 0) 
+            {
+                player_rb.velocity = transform.forward * -speed;
+                animator.Play("AimWalkBack");
+                animator.SetFloat("movementSpeed", direction.magnitude);
+
+            }
+            if (vertical > 0)
+            {
+                player_rb.velocity = transform.forward * speed;
+                animator.Play("AimWalkForward");
+                animator.SetFloat("movementSpeed", direction.magnitude);
+
+            }
+
+        }
+        if (estaApuntando)
+        {
+            float rotationSpeed = 6f;
+            player_rb.rotation = Quaternion.Euler(player_rb.rotation.eulerAngles + new Vector3(0f, rotationSpeed * Input.GetAxis("Mouse X"), 0f));
+            espinaDorsal.transform.rotation = Quaternion.Euler(espinaDorsal.transform.rotation.eulerAngles + new Vector3(rotationSpeed * Input.GetAxis("Mouse Y"), 0f, 0f));
+
+           // Mathf.Clamp(player_rb.rotation.eulerAngles.y, 5f, 30f);
+        }
+
 
 
     }
